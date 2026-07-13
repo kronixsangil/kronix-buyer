b523e6c828c0f2cbd8e4df18c14ff253f211de5d
kronixsangil <blassena@hotmail.com>
2026-07-12 20:49:11 -0500

    Dinamic Service

--- app/(buyer)/kronix/enviar/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/(buyer)/kronix/enviar/page.tsx
-"use client";
-
-import GenericTransportServiceRequest from "@/components/buyer/kronix/GenericTransportServiceRequest";
-import { getTransportServiceConfig } from "@/lib/services/transportServices";
-
-export default function KronixEnviarPage() {
-  return <GenericTransportServiceRequest config={getTransportServiceConfig("enviar")} />;
-}
